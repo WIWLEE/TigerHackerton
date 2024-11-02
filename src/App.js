24f037b77e2914392a2086bb00c7dbae9fe3014a
JoungYujin <jyjin0205@postech.ac.kr>
2024-11-02 14:57:46 -0500

    Made camera page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 import './App.css';
 import Home from './Home.js';
 import RegisterQR from './RegisterQR.js';
 import ViewFoodList from './ViewFoodList.js';
+import CameraPage from './CameraPage.js';
 
 export default class App extends Component {
   state = {
@@ -37,40 +38,6 @@ export default class App extends Component {
     });
   };
 
-  submitQRInfo = () => {
-    const requestData = {
-        name: 'Cucumber',
-        food_description: 'Fresh cucumber for salads.',
-        delivery_company_ID: 1,
-        handling_description: 'Keep refrigerated.',
-        entry_date: '2024-01-01',
-        disposal_date: '2024-01-15',
-        stock_quantity: 50,
-        storage_location: 'Storage Room 1'
-    };
-
-    fetch("http://localhost:3001/info", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json", // Capitalize 'Content-Type'
-        },
-        body: JSON.stringify(requestData)
-    })
-    .then(res => {
-        if (!res.ok) {
-            throw new Error(`HTTP error! Status: ${res.status}`);
-        }
-        return res.json(); // Parse the JSON response
-    })
-    .then(data => {
-        console.log('Success:', data); // Handle the response data
-    })
-    .catch(error => {
-        console.error('Error:', error); // Log the error
-    });
-};
-
-
   onCall =()=>{
     fetch("http://localhost:3001/callbody",{
       method:"post",
@@ -93,13 +60,12 @@ export default class App extends Component {
         <nav>
           <Link to="ViewFoodList">ViewFoodList</Link> | <Link to="RegisterQR">RegisterQR</Link>
         </nav>
-        <Routes>
-          <Route 
-              path="/" 
-              element={<Home submitQRInfo={this.submitQRInfo} />} // submitQRInfo 함수를 props로 전달
-            />
-          {/* <Route path="/about" element={<About />} /> */}
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ViewFoodList"  element={<ViewFoodList />} />
+        <Route path="/RegisterQR" element={<RegisterQR />} />
+        <Route path="/Camera" element={<CameraPage />} />
+      </Routes>
       </div>
     </Router>
       // <div>
